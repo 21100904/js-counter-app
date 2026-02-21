55d47044f0fbb76795f3599688abfaf5195e4901
21100904 <khuthiemuzila@gmail.com>
2026-02-21 18:52:54 +0200

    Enhance counter app
    Enhance counter app with storage and UI improvements

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # js-counter-app
-A simple Javascript counter  app
+A simple Javascript counter  app built with HTML, CSS, and JS
+
+### Features
+- Increment counter
+- Decrement counter
+- Reset counter
+- Prevents negative changes
+- Saves count using local storage
+
+### What I learned
+- DOM manipulation
+- Event Listeners
+- Local Storage
+- GitHub workflow
--- script.js
+++ script.js
@@ -1,20 +1,39 @@
-let count = 0;
+let count = localStorage.getItem('count');
+count = count ? Number(count) 
+: 0;
+
+
 const countDisplay = document.getElementById('count');
 const incrementBtn = document.getElementById('increment');
 const decrementBtn = document.getElementById('decrement');
 const resetBtn = document.getElementById('reset');
 
+function updateColor() {
+    if (count > 0) {
+        countDisplay.style.color = 'green';
+    } else {
+        countDisplay.style.color = 'black';
+    }}
+
 incrementBtn.addEventListener('click', () => {
-    count = count + 1;
+    count = count ++;
+    localStorage.setItem('count', count);
     countDisplay.textContent = count;
+    updateColor();
 });
 
 decrementBtn.addEventListener('click', () => {
-    count = count - 1;
+    if (count > 0) {
+        count--;
+        localStorage.setItem('count', count);
     countDisplay.textContent = count;
+    updateColor();
+    }
 });
 
 resetBtn.addEventListener('click', () => {
     count = 0;
+    localStorage.setItem('count', count);
     countDisplay.textContent = count;
+    updateColor();
 });
